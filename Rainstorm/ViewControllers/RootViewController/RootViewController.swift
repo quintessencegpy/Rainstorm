d7dee5e2c5dde943568d404c9acbe5705ec84469
goupy <goupy@fenbi.com>
2020-07-11 19:56:43 +0800

    Project setup

--- Rainstorm/ViewControllers/RootViewController/RootViewController.swift
+++ Rainstorm/ViewControllers/RootViewController/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  Rainstorm
 //
 //  Created by Pengyu Gou on 7/11/20.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class RootViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
